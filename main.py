2264cae515cf0cc37700387727c004917504211c
Mohana M <145038131+somuchmoh@users.noreply.github.com>
2024-01-02 00:53:54 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+app.secret_key = "NsWp[f138fw%$sdq!$R"
+
+
+class MyForm(FlaskForm):
+    email = StringField(label='email', validators=[Email()])
+    password = PasswordField(label='password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label='Log In')
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = MyForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@email.com" and form.password.data == "12345678":
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
+    return render_template('login.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
